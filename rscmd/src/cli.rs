afe2440df344c8ef115ae6aa2589bebd4d3343c1
András El Koulali <deak311andris@gmail.com>
2021-03-08 21:00:29 +0100

    Start to optimize binary size
    Scrap out log dependencies and introduce optimization options in
    cargo.toml.
    
    Replace log functions in rscmd's error handling routine with
    custom-written ones.

--- rscmd/src/cli.rs
+++ rscmd/src/cli.rs
@@ -12,7 +12,8 @@ fn style_if_term(style: ansi_term::Style) -> ansi_term::Style {
 lazy_static! {
     pub static ref USE_COLOR: bool =
         atty::is(atty::Stream::Stdout) && atty::is(atty::Stream::Stderr);
-    pub static ref ERROR_HEADER_STYLE: ansi_term::Style = style_if_term(Color::Yellow.bold());
+    pub static ref ERROR_HEADER_STYLE: ansi_term::Style = style_if_term(Color::Red.bold());
     pub static ref WARNING_HEADER_STYLE: ansi_term::Style = style_if_term(Color::Yellow.bold());
     pub static ref SECONDARY: ansi_term::Style = style_if_term(Color::Cyan.bold());
+    pub static ref HIGHLIGHT: ansi_term::Style = style_if_term(Color::White.bold());
 }
--- rscmd/src/main.rs
+++ rscmd/src/main.rs
@@ -1,6 +1,3 @@
-extern crate pretty_env_logger;
-#[macro_use]
-extern crate log;
 extern crate rustone;
 
 use clap::{load_yaml, App};
@@ -14,8 +11,6 @@ fn enable_ansi() {
 }
 
 fn main() {
-    pretty_env_logger::init();
-
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from_yaml(yaml).get_matches();
 
@@ -32,26 +27,28 @@ fn main() {
     enable_ansi();
 
     if let Err(err) = out {
-        error!("Error! {}", err);
+        println!("{} {}", cli::ERROR_HEADER_STYLE.paint("error!"), err);
 
         for e in err.iter().skip(1) {
-            error!("caused by: {}", e);
+            println!(" {} {}", cli::HIGHLIGHT.paint("caused by:"), e);
         }
 
         // Manual backtrace implementation, don't ask please
         if let Some(backtrace) = err.backtrace() {
-            error!("backtrace: ");
+            println!("backtrace: ");
             for (i, frame) in backtrace.frames().iter().enumerate() {
-                error!("{}:", i);
+                println!(" {}:", i);
                 for symbol in frame.symbols() {
-                    error!(
-                        " - {} @ {} : {}",
-                        symbol.name().map_or_else(
-                            || symbol
-                                .addr()
-                                .map_or_else(|| String::from("?"), |a| format!("{:p}", a)),
+                    println!(
+                        "  - {} @ {} : {}",
+                        cli::HIGHLIGHT.paint(symbol.name().map_or_else(
+                            || {
+                                symbol
+                                    .addr()
+                                    .map_or_else(|| String::from("?"), |a| format!("{:p}", a))
+                            },
                             |n| n.to_string()
-                        ),
+                        )),
                         symbol
                             .filename()
                             .map_or_else(|| "?", |n| n.to_str().unwrap_or("?")),
